9153b2bf2f895253c3ee4fcde0a4f32e8ff36bff
Andrew <expandrew@gmail.com>
2017-06-13 20:55:27 -0700

    pass our second test, which checks that the method properly counts the instances of a word in a string.

--- Objects/WordCount.cs
+++ Objects/WordCount.cs
@@ -14,18 +14,18 @@ namespace WordCounter
       _countWord = countWord;
     }
 
-    // public int CountRepeat(string inString, string countWord)
-    // {
-    //   int repeats = 0;
-    //   string[] stringWords = inString.Split(' ');
-    //   foreach (string word in stringWords)
-    //   {
-    //     if (inString.Contains(countWord))
-    //     {
-    //       repeats += 1;
-    //     }
-    //   }
-    //   return repeats;
-    // }
+    public int CountRepeat(string inString, string countWord)
+    {
+      int repeats = 0;
+      string[] stringWords = inString.Split(' ');
+      foreach (string word in stringWords)
+      {
+        if (inString.Contains(countWord))
+        {
+          repeats += 1;
+        }
+      }
+      return repeats;
+    }
   }
 }
--- Tests/WordCountTests.cs
+++ Tests/WordCountTests.cs
@@ -17,26 +17,15 @@ namespace WordCounter.Objects
       Assert.Equal(true, contains);
     }
 
-    // [Fact]
-    // public void Splits_String_Into_Words()
-    // {
-    //   //Arrange, Act
-    //   string testString = "This is a TestWord";
-    //   string[] words = testString.Split(default(string[]), StringSplitOptions.RemoveEmptyEntries);
-    //   string[] testArray = {"This", "is", "a", "TestWord"};
-    //   //Assert
-    //   Assert.Equal(testArray, words);
-    // }
-
-    // [Fact]
-    // public void Counts_Word_Instances()
-    // {
-    //   //Arrange
-    //   RepeatCounter testInstance = new RepeatCounter("TestWord TestWord TestWord", "TestWord");
-    //   //Act
-    //   int Repeats = testInstance.CountRepeat("TestWord TestWord TestWord", "TestWord");
-    //   //Assert
-    //   Assert.Equal(3, Repeats);
-    // }
+    [Fact]
+    public void Counts_Word_Instances()
+    {
+      //Arrange
+      RepeatCounter testInstance = new RepeatCounter("TestWord TestWord TestWord", "TestWord");
+      //Act
+      int Repeats = testInstance.CountRepeat("TestWord TestWord TestWord", "TestWord");
+      //Assert
+      Assert.Equal(3, Repeats);
+    }
   }
 }
